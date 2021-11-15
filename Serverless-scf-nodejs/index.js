df8fafcabe92614de89b6665114a474d5bbaaaa5
胡争辉 <hu@daonao.com>
2021-11-15 12:38:32 +0800

    add Serverless-scf-nodejs

---
+++ Serverless-scf-nodejs/index.js
@@ -0,0 +1,10 @@
+'use strict';
+exports.main_handler = async (event, context) => {
+    console.log("Hello World")
+    console.log(event)
+    console.log(event["non-exist"])
+    console.log(context)
+    event["Serverless-app"] = "Serverless-scf-nodejs"
+    event["Serverless-name"] = "scf-nodejs"
+    return event
+};
